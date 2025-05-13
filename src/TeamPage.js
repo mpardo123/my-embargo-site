a009b1efd1c61eaba26f22b786a8b9da0a09c2c6
Michael Pardo <michaelpardo@michaels-air-2.mynetworksettings.com>
2025-05-13 12:50:46 -0400

    Updated TeamPage.js to fetch and display team-specific player data with dynamic styling

--- src/TeamPage.js
+++ src/TeamPage.js
@@ -7,17 +7,17 @@ function TeamPage({ teamName }) {
 
   // Define team colors
   const teamColors = {
-    "All Season Services": { primary: "#b2b6b7", secondary: "#e888a3", logo: "/logos/All Season Services.jpg" },
+    "All Season Services": { primary: "#b2b6b7", secondary: "#e888a3", logo: "/logos/All_Season_Services.jpg" },
     "Amp'd Up": { primary: "#e41616", secondary: "#000000", logo: "/logos/ampd.png" },
     "Buzzards Bay Basins": { primary: "#1e633f", secondary: "#000000", logo: "/logos/basins.jpg" },
     "Better Way Primary Care": { primary: "#acc5d7", secondary: "#8abf6f", logo: "/logos/bwpc.png" },
     "Cornerstone Financial": { primary: "#c0bcdc", secondary: "#3a3c92", logo: "/logos/Cornerstone.jpg" },
-    "Cantara Brothers Co": { primary: "#e888a3", secondary: "#000000", logo: "/logos/Minkle Boys.png" },
+    "Cantara Brothers Co": { primary: "#e888a3", secondary: "#000000", logo: "/logos/Cantara_Brothers_Co.png" },
     "OSS Landscape": { primary: "#373879", secondary: "#b1b7b7", logo: "/logos/oss.jpg" },
     "Wash Ashore": { primary: "#ede4c0", secondary: "#689bbc", logo: "/logos/washashore.png" },
-    "Mattapoisett PD": { primary: "#000000", secondary: "#0e4769", logo: "/logos/Matt PD.jpg" },
-    "Minkle Boys / SPM": { primary: "#ffe327", secondary: "#000000", logo: "/logos/Minkle Boys.png" },
-    "River Junction": { primary: "#fa690c", secondary: "#000000", logo: "/logos/River Junction.png" },
+    "Mattapoisett PD": { primary: "#000000", secondary: "#0e4769", logo: "/logos/Matt_PD.jpg" },
+    "Minkle Boys / SPM": { primary: "#ffe327", secondary: "#000000", logo: "/logos/Minkle_Boys.png" },
+    "River Junction": { primary: "#fa690c", secondary: "#000000", logo: "/logos/River_Junction.png" },
     "Rock Electric": { primary: "#012a52", secondary: "#e25f28", logo: "/logos/rock.png" },
     "Traveler's Alehouse": { primary: "#b89d52", secondary: "#000000", logo: "/logos/travelers.jpeg" },
     "Tropical Smoothie Cafe": { primary: "#00b0de", secondary: "#da3987", logo: "/logos/trops.jpeg" }
@@ -27,7 +27,7 @@ function TeamPage({ teamName }) {
   const teamColor = teamColors[teamName] || { primary: "#000000", secondary: "#FFFFFF", logo: "/logos/default-logo.png" };
 
   useEffect(() => {
-    const playerCardApiUrl = `https://sheets.googleapis.com/v4/spreadsheets/1aRRv1kR7BweWa7GZInAncIvpWwks0Ttm7_WAz5FWpRA/values/PlayerCard!A1:Z1000?key=AIzaSyAQQPxbPiu32yVmxzQMp9YCQWCGWa4uVNo`;
+    const playerCardApiUrl = `https://sheets.googleapis.com/v4/spreadsheets/1aRRv1kR7BweWa7GZInAncIvpWwks0Ttm7_WAz5FWpRA/values/PlayerCard!A1:Z1000?key=${process.env.REACT_APP_GOOGLE_SHEETS_API_KEY}`;
 
     fetch(playerCardApiUrl)
       .then(response => {
