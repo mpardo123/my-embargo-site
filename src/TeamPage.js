8ee9c51f6bfdffa111f908ed7c5e1a73fde2c985
Michael Pardo <michaelpardo@michaels-air-2.mynetworksettings.com>
2025-05-13 18:57:41 -0400

    Updated TeamPage.js to display team details, player stats, and dynamic team logos

--- src/TeamPage.js
+++ src/TeamPage.js
@@ -8,24 +8,24 @@ function TeamPage({ teamName }) {
 
   // Define team colors
   const teamColors = {
-    "All Season Services": { primary: "#b2b6b7", secondary: "#e888a3", logo: "/Logos/All_Season_Services.jpg" },
-    "Amp'd Up": { primary: "#e41616", secondary: "#000000", logo: "/Logos/ampd.png" },
-    "Buzzards Bay Basins": { primary: "#1e633f", secondary: "#000000", logo: "/Logos/basins.jpg" },
-    "Better Way Primary Care": { primary: "#acc5d7", secondary: "#8abf6f", logo: "/Logos/bwpc.png" },
-    "Cornerstone Financial": { primary: "#c0bcdc", secondary: "#3a3c92", logo: "/Logos/Cornerstone.jpg" },
-    "Cantara Brothers Co": { primary: "#e888a3", secondary: "#000000", logo: "/Logos/Cantara_Brothers_Co.png" },
-    "OSS Landscape": { primary: "#373879", secondary: "#b1b7b7", logo: "/Logos/oss.jpg" },
-    "Wash Ashore": { primary: "#ede4c0", secondary: "#689bbc", logo: "/Logos/washashore.png" },
-    "Mattapoisett PD": { primary: "#000000", secondary: "#0e4769", logo: "/Logos/Matt_PD.jpg" },
-    "Minkle Boys / SPM": { primary: "#ffe327", secondary: "#000000", logo: "/Logos/Minkle_Boys.png" },
-    "River Junction": { primary: "#fa690c", secondary: "#000000", logo: "/Logos/River_Junction.png" },
-    "Rock Electric": { primary: "#012a52", secondary: "#e25f28", logo: "/Logos/rock.png" },
-    "Traveler's Alehouse": { primary: "#b89d52", secondary: "#000000", logo: "/Logos/travelers.jpeg" },
-    "Tropical Smoothie Cafe": { primary: "#00b0de", secondary: "#da3987", logo: "/Logos/trops.jpeg" }
+    "All Season Services": { primary: "#b2b6b7", secondary: "#e888a3", Logo: "/Logos/All_Season_Services.jpg" },
+    "Amp'd Up": { primary: "#e41616", secondary: "#000000", Logo: "/Logos/ampd.png" },
+    "Buzzards Bay Basins": { primary: "#1e633f", secondary: "#000000", Logo: "/Logos/basins.jpg" },
+    "Better Way Primary Care": { primary: "#acc5d7", secondary: "#8abf6f", Logo: "/Logos/bwpc.png" },
+    "Cornerstone Financial": { primary: "#c0bcdc", secondary: "#3a3c92", Logo: "/Logos/Cornerstone.jpg" },
+    "Cantara Brothers Co": { primary: "#e888a3", secondary: "#000000", Logo: "/Logos/Cantara_Brothers_Co.png" },
+    "OSS Landscape": { primary: "#373879", secondary: "#b1b7b7", Logo: "/Logos/oss.jpg" },
+    "Wash Ashore": { primary: "#ede4c0", secondary: "#689bbc", Logo: "/Logos/washashore.png" },
+    "Mattapoisett PD": { primary: "#000000", secondary: "#0e4769", Logo: "/Logos/Matt_PD.jpg" },
+    "Minkle Boys / SPM": { primary: "#ffe327", secondary: "#000000", Logo: "/Logos/Minkle_Boys.png" },
+    "River Junction": { primary: "#fa690c", secondary: "#000000", Logo: "/Logos/River_Junction.png" },
+    "Rock Electric": { primary: "#012a52", secondary: "#e25f28", Logo: "/Logos/rock.png" },
+    "Traveler's Alehouse": { primary: "#b89d52", secondary: "#000000", Logo: "/Logos/travelers.jpeg" },
+    "Tropical Smoothie Cafe": { primary: "#00b0de", secondary: "#da3987", Logo: "/Logos/trops.jpeg" }
   };
 
   // Get the team's colors or use default colors
-  const teamColor = teamColors[teamName] || { primary: "#000000", secondary: "#FFFFFF", logo: "/logos/default-logo.png" };
+  const teamColor = teamColors[teamName] || { primary: "#000000", secondary: "#FFFFFF", Logo: "/Logos/default-Logo.png" };
 
   useEffect(() => {
     const playerCardApiUrl = `https://sheets.googleapis.com/v4/spreadsheets/1aRRv1kR7BweWa7GZInAncIvpWwks0Ttm7_WAz5FWpRA/values/PlayerCard!A1:Z1000?key=${process.env.REACT_APP_GOOGLE_SHEETS_API_KEY}`;
@@ -85,14 +85,14 @@ function TeamPage({ teamName }) {
     >
       <div className="team-header" style={{ marginBottom: "20px" }}>
         <img
-          src={teamColor.logo}
+          src={teamColor.Logo}
           alt={`${teamName} Logo`}
-          className="team-logo"
+          className="team-Logo"
           style={{
-            width: "100px", // Set a fixed width for the logo
-            height: "100px", // Set a fixed height for the logo
-            objectFit: "contain", // Ensure the logo fits within the box
-            marginBottom: "10px", // Add spacing below the logo
+            width: "100px", // Set a fixed width for the Logo
+            height: "100px", // Set a fixed height for the Logo
+            objectFit: "contain", // Ensure the Logo fits within the box
+            marginBottom: "10px", // Add spacing below the Logo
           }}
         />
         <h1
